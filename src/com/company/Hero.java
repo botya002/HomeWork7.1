f4f2d24480c48a5799eae88c9928a1dfdf0c86a0
botya02 <botagejm@gmail.com>
2021-09-15 12:56:23 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public abstract class Hero {
+    private int HP;
+    private int damage;
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
